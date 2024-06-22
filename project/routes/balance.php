e2782d01f07d927c56fcdc7537aff04d80ca46ad
endlesswp <162703175+endlesswp@users.noreply.github.com>
2024-06-22 23:36:20 +0700

    Delete project/routes/balance.php
    digabung ke web

--- project/routes/balance.php
+++
@@ -1,6 +0,0 @@
-<?php
-use App\Http\Controllers\BalanceController;
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/check-balance', [BalanceController::class, 'checkBalance']);
-});
