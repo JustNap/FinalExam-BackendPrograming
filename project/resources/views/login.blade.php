53d579249136422362d2eb8e231414364462a981
JustNap <justinvalen04@gmail.com>
2024-06-26 23:51:00 +0700

    membuat no rekening

--- project/resources/views/login.blade.php
+++ project/resources/views/login.blade.php
@@ -22,16 +22,18 @@
         </div>
     </div>
 
-    <div>
-        <div class="flex items-center justify-between">
-        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+    <div class="relative">
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        <div class="password-wrapper mt-2 relative">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <svg id="togglePassword" class="absolute right-0 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5a9 9 0 00-1.715 17.69A8.962 8.962 0 009 19c5 0 9-4 9-9s-4-9-9-9z"/>
+            </svg>
         </div>
-        <div class="mt-2">
-        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-        <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-        </div>
+    </div>
+    <div class="text-sm">
+        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
     </div>
 
     <div>
@@ -46,5 +48,23 @@
     </p>
 </div>
 </div>
+
+<script>
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function() {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        if (type === 'text') {
+            togglePassword.classList.remove('text-gray-400');
+            togglePassword.classList.add('text-indigo-600');
+        } else {
+            togglePassword.classList.remove('text-indigo-600');
+            togglePassword.classList.add('text-gray-400');
+        }
+    });
+</script>
 </body>
 </html>
